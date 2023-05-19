b5118763b16d23641e2b69bea788d1b587724a5c
felixesteban100 <felixesteban100@gmail.com>
2023-05-18 20:26:04 -0400

    add changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import SideNav from "~/components/SideNav";
+
+
+// we left the video tutorial there: https://youtu.be/jqVm5_G1ZEE?t=6072
+
+// Pages using during the development process
+
+// https://app.planetscale.com/felixesteban100/social-app/deploy-requests/2
+// https://vercel.com/felixesteban100/social-app
+// http://localhost:3000
+// https://social-app-alpha-navy.vercel.app
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +24,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter Clone</title>
+        <meta name="description" content="This is a Twitter clone" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+
+        </div>
+      </div>
     </SessionProvider>
   );
 };
